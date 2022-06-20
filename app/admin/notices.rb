f2b03c1671433a7acef4ba446bf3d381cf8e0465
Ruben Homs <ruben@homs.codes>
2022-06-20 16:20:48 +0200

    Add archived scope and index changes for activeadmin of Video and Notice

--- app/admin/notices.rb
+++ app/admin/notices.rb
@@ -1,3 +1,18 @@
 ActiveAdmin.register Notice do
+  menu priority: 2, label: "Notice board"
   permit_params :title, :description, :contact_info, :user_id
+  scope 'Active', :active, default: true
+  scope 'Archived', :archived
+
+  index do
+    selectable_column
+    column :title
+    column :descripion
+    column :contact_info
+    actions
+  end
+
+  filter :title
+  filter :description
+  filter :contact_info
 end
--- app/admin/videos.rb
+++ app/admin/videos.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register Video do
+  menu priority: 1
   permit_params :title, :url
+  scope 'Active', :active, default: true
+  scope 'Archived', :archived
+
+  index do
+    selectable_column
+    column :title
+    column {|video| link_to "Watch", video.url }
+    actions
+  end
+
+  filter :title
+  filter :url
 end
